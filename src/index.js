6f7e3476375b93125d40ecb222bd08b432bdaef2
Alex Dunstan Kiiru Mureithi <alexkiiru7@gmail.com>
2025-03-21 14:54:56 +0300

    completes challenge 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 console.log('%c HI', 'color: firebrick')
+
+//challenge 1
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+
+    fetch(imgUrl)
+        .then(response => response.json())
+        .then(data => {
+            const imgContainer = document.getElementById("dog-image-container");
+            data.message.forEach(imgUrl => {
+                const img = document.createElement("img");
+                img.src = imgUrl;
+                img.alt = "A Dog Image";
+                imgContainer.appendChild(img);
+            });
+        })
+
+        .catch(error => console.log("Error fetching images:", error));
+
+})''
